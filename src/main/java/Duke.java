e44570b08b2a0e607270c9761038ae3812fe5642
broccoli-hater <ihatebroccoli404@gmail.com>
2025-02-09 10:06:12 +0800

    Add GUI components

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,4 +2,26 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+//    private String commandType;
+//    public String getResponse(String input) {
+//        try {
+//            Command c = Parser.parse(input);
+//            c.execute(tasks, ui, storage);
+//            commandType = c.getClass().getSimpleName();
+//            return c.getString();
+//        } catch (DukeException e) {
+//            return "Error: " + e.getMessage();
+//        }
+//    }
+//    public String getCommandType() {
+//        return commandType;
+//    }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            stage.setTitle("Sir Talks-A-Lot");
+
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
